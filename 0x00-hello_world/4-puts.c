02588e51a53fca3740c4fa8d90d257610ca021b3
Wanjirukamande <mayrkamande@gmail.com>
2023-06-18 17:21:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
-#!/bin/bash
 #include <stdio.h> 
 /**
- * main - A program that prints a line with fputs function
+ * main - A program that prints a line with puts function
  * Return: 0 (Success)
  */ 
 int main(void)
